0a264714ff4e54e6b808dcadac22d32bf32f41f8
Kendall <kendallawills@gmail.com>
2023-07-23 16:10:06 -0400

    Step 1

---
+++ secret.js
@@ -0,0 +1,8 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 
+'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015', 'Chris', 'Pine,', 
+'Learn', 'JavaScript'];
+
+// Remove last string
+
+secretMessage.pop();
+
